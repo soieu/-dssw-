2cf41bc4aedb320fda1fb2c81ba8c8711e13d8d7
soieu <soieu9898@gmail.com>
2023-03-27 17:26:20 +0900

    230327

---
+++ 02.HTMLCSSJS/03.JS/01.script-2/change-color.js
@@ -0,0 +1,5 @@
+var heading = document.querySelector("#heading"); //id가 heading인 요소를 heading 변수에 대입
+heading.onclick = function () {
+	// heading을 클릭하면 함수 실행
+	heading.style.color = "red"; // heading의 style 중 color를 "red"로 변경
+};
